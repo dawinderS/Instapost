12a6e053db5c99cdfd0eaae4b81caeae7f7abb56
Dawinder Singh <dawindernsingh@gmail.com>
2020-07-14 12:40:51 -0700

    add basic frontend setup and routes

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = "1px solid #e6e6e6";
+const BORDER_RADIUS = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f6",
+  darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BORDER_RADIUS};
+             ${BOX_BORDER};
+             background-color:white; 
+            `
+};
