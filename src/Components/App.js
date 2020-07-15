97cfcf5641994fe68124dd807346135c0b079ca2
Dawinder Singh <dawindernsingh@gmail.com>
2020-07-15 15:54:34 -0700

    establish auth page w/ login and signup

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from '../Styles/GlobalStyles';
 import Theme from '../Styles/Theme';
 import Router from './Router';
 import { useQuery } from 'react-apollo-hooks';
+import Footer from './Footer';
 
 const QUERY = gql`
   {
     isLoggedIn @client
   }
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${(props) => props.theme.maxWidth};
+  width: 100%;
 `;
 
 export default () => {
@@ -18,10 +25,11 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <>
+      <Wrapper>
         <GlobalStyles />
         <Router isLoggedIn={isLoggedIn} />
-      </>
+        <Footer />
+      </Wrapper>
     </ThemeProvider>
   );
 };
